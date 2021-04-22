f541f5c68a09d0761869a28be0d5c5a5e7da34d7
harshjoshi19 <harshjoshijee12@gmail.com>
2021-04-22 14:27:47 +0530

    session fetch in database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
@@ -8,7 +9,58 @@ const expressLayout = require("express-ejs-layouts");
 const { static } = require("express");
 
 var PORT = process.env.PORT || 3300;
+var mongoose = require("mongoose");
 
+const flash = require("express-flash");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+//Database connection
+//For all project this can be Same  ...for remind
+const url = "mongodb://localhost/pizza";
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+const connection = mongoose.connection;
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .catch((err) => {
+    console.log("Connection failed...");
+  });
+
+// session store
+var store = new MongoDBStore({
+  uri: url,
+  collection: "sessions",
+});
+
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
+
+// session config
+app.use(
+  require("express-session")({
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 week
+    },
+    store: store,
+    // Boilerplate options, see:
+    // * https://www.npmjs.com/package/express-session#resave
+    // * https://www.npmjs.com/package/express-session#saveuninitialized
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
 //asset
 app.use(express.static("public"));
 
@@ -20,18 +72,4 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT} `);
 });
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/login", (req, res) => {
-  res.render("auth/login");
-});
-
-app.get("/register", (req, res) => {
-  res.render("auth/register");
-});
-
-app.get("/cart", (req, res) => {
-  res.render("customers/cart");
-});
+require("./routes/web")(app);
